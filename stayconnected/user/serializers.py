736e68534499500c8feb6d9d6787607432d77c0e
Mariam Khokhiashvili <142008382+MariamKhoKh@users.noreply.github.com>
2024-12-05 00:01:45 +0400

    resolve repeat password issue

--- stayconnected/user/serializers.py
+++ stayconnected/user/serializers.py
@@ -7,10 +7,11 @@ User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    repeat_password = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'created_at', 'status']
+        fields = ['username', 'email', 'password', 'repeat_password', 'created_at', 'status']
         extra_kwargs = {'password': {'write_only': True}}
 
     def validate(self, data):
