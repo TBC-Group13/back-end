27ff9408b21679a4abde27af4686a44e50dc2ddb
nnaeyno <ngogo20@freeuni.edu.ge>
2024-12-07 16:22:22 +0400

    url

--- stayconnected/qa/urls.py
+++ stayconnected/qa/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import (
     QuestionListCreateAPIView, AnswerListCreateAPIView,
     LikeDislikeAnswerAPIView, MarkCorrectAnswerAPIView, TagListCreateAPIView, SearchAPIView, QuestionAnswersListView,
+    UserQuestionListAPIView,
 )
 
 urlpatterns = [
@@ -12,4 +13,11 @@ urlpatterns = [
     path('tags/', TagListCreateAPIView.as_view(), name='tag-list-create'),
     path('questions/search/', SearchAPIView.as_view(), name='search-questions'),
     path('questions/<int:question_id>/list-answers/', QuestionAnswersListView.as_view(), name='question-answers'),
+    path('personal/questions/', UserQuestionListAPIView.as_view(), name='user-questions'),
 ]
+
+"""
+curl -X GET http://127.0.0.1:8000/api/personal/questions/ \
+     -H "Content-Type: application/json" \
+     -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzMzNTc2NDk1LCJpYXQiOjE3MzM1NzI4OTUsImp0aSI6ImNmNTg4YmNkZGFkMTQ4MzZiNmRhNjU1ZmQ2Mjg5MDA4IiwidXNlcl9pZCI6MX0.fED_pREN9kesMiXXPIXe5kQFKUjKB9UrAuaJHlb3uyc"
+"""
